efa09def24ae9f103ea947db9cfd9ebffb30fde0
Vatsal Manot <vatsal.manot@yahoo.com>
2024-10-03 21:15:35 +0530

    Update preternatural.rb

--- preternatural.rb
+++ preternatural.rb
@@ -1,5 +1,5 @@
 class Preternatural < Formula
-     desc "Preternatural CLI tool"
+     desc "Preternatural CLI Tool"
      homepage "https://github.com/PreternaturalAI/hombrew-preternatural"
      url "https://github.com/PreternaturalAI/cli-binary/raw/refs/heads/master/Sources/preternatural"
      sha256 "d6bead16941c3e4f24b8cb645443ba2dd7e948c1e8179a41e2f7e1faaf09d70a"
