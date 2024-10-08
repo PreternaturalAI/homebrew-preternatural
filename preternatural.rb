860cd4cca9d7ee42c399348ba2aad8ee57e1b419
Akshat Patel <akshat.patel16@gmail.com>
2024-10-08 17:06:14 +0530

    Update preternatural.rb

--- preternatural.rb
+++ preternatural.rb
@@ -1,7 +1,7 @@
 class Preternatural < Formula
      desc "Preternatural CLI Tool"
      homepage "https://github.com/PreternaturalAI/hombrew-preternatural"
-     url "https://github.com/PreternaturalAI/cli-binary/raw/refs/heads/master/Sources/preternatural"
+     url "https://github.com/PreternaturalAI/CLI-release/raw/refs/heads/master/Sources/preternatural"
      sha256 "8291652f023ac0d6bf8f44535055809f9b7dd6f0f5bea9ce9bf96a3c304d228d"
      version "0.0.29"
 
