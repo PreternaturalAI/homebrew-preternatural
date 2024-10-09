777cd1cdc274cbe2977e1e66f63a7c37407d7957
Akshat Patel <akshat.patel16@gmail.com>
2024-10-09 17:14:21 +0530

    Update

--- preternatural.rb
+++ preternatural.rb
@@ -1,15 +1,16 @@
 class Preternatural < Formula
-     desc "Preternatural CLI Tool"
-     homepage "https://github.com/PreternaturalAI/hombrew-preternatural"
-     url "https://github.com/PreternaturalAI/CLI-release/raw/refs/heads/master/Sources/preternatural"
-     sha256 "9e910e2b69eb39eb01896a17ea0e188a5c753ca19ba2ebc67042d41269bf5391"
-     version "0.0.30"
+  desc "Preternatural CLI Tool"
+  homepage "https://github.com/PreternaturalAI/hombrew-preternatural"
+  url "https://github.com/PreternaturalAI/CLI-release/releases/download/0.0.30/preternatural.artifactbundle.zip"
+  sha256 "3a057795fce77aa3b591bcb9332c171aa588dc492768e6e5a1c1c61b552407ab"
+  version "0.0.30"
 
-     def install
-       bin.install "preternatural"
-     end
+  def install
+    system "unzip", "-o", cached_download
+    bin.install "preternatural/bin/preternatural"
+  end
 
-     test do
-       system "#{bin}/preternatural"
-     end
-   end
+  test do
+    system "#{bin}/preternatural"
+  end
+end
