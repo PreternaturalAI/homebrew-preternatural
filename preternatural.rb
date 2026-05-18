55e52b23fa665fc0c53fee7cb390e38321bd4f7b
Vatsal Manot <5306992+vmanot@users.noreply.github.com>
2026-05-18 10:57:16 +0530

    Update preternatural to version 0.0.25

--- preternatural.rb
+++ preternatural.rb
@@ -1,9 +1,9 @@
 class Preternatural < Formula
   desc "Preternatural CLI Tool"
   homepage "https://github.com/PreternaturalAI/homebrew-preternatural"
-  url "https://github.com/PreternaturalAI/homebrew-preternatural/releases/download/preternatural-0.0.24/final-artifact.zip"
-  sha256 "1c3f8ceef5e83bd5287ea801d1310099a8083aa7e4cbbf8b6ea147024a2ce61c"
-  version "0.0.24"
+  url "https://github.com/PreternaturalAI/homebrew-preternatural/releases/download/preternatural-0.0.25/final-artifact.zip"
+  sha256 "1de204e24bdde5911f93a910896c36333beb3f1e85270da13bd11359d6507f4e"
+  version "0.0.25"
 
   def install
     # Unzip the main artifact bundle
