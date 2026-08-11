cb9344bbc2b3b9596b2ba3e0c3d769713583a0ef
Vatsal Manot <5306992+vmanot@users.noreply.github.com>
2026-08-11 21:31:58 +0530

    Release browser-extension 0.1.0

--- Casks/browser-extension.rb
+++ Casks/browser-extension.rb
@@ -2,7 +2,7 @@ cask "browser-extension" do
   version "0.1.0"
   sha256 "3e7d6f72f5a4b53982fb6b18caf039747c6ba85ac5a472f313648bc6869d5c74"
 
-  url "https://github.com/PreternaturalAI/homebrew-preternatural/releases/download/browser-extension-0.1.0/browser-extension.zip"
+  url "https://github.com/PreternaturalAI/homebrew-preternatural/releases/download/browser-extension-#{version}/browser-extension.zip"
   name "Preternatural Browser Extension"
   desc "Native host and Safari extension for Preternatural browser automation"
   homepage "https://preternatural.ai"
