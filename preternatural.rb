4bdd8ad56e3c98ebe91c8b5591e93f21290bda1b
Vatsal Manot <5306992+vmanot@users.noreply.github.com>
2026-08-12 20:49:24 +0530

    Release preternatural 0.0.56

--- preternatural.rb
+++ preternatural.rb
@@ -1,33 +1,13 @@
 class Preternatural < Formula
   desc "Command-line development automation tool"
-  homepage "https://github.com/PreternaturalAI/homebrew-preternatural"
-  url "https://github.com/PreternaturalAI/homebrew-preternatural/releases/download/preternatural-0.0.56/final-artifact.zip"
-  sha256 "5db4cb80a792ad864f95b22bff91869cf7e361d9b53b73d8dcbc81d14b64b352"
+  homepage "https://github.com/PreternaturalAI/command-line-tool"
+  url "https://github.com/PreternaturalAI/homebrew-preternatural/releases/download/preternatural-0.0.56/preternatural.zip"
+  sha256 "dc262c368556ffef0c303c76e5cc4711d8ccd9d86878f9066795895ec7442997"
+  depends_on macos: :sonoma
 
   def install
-    artifact_root = buildpath/"swift-brew-artifacts"
-    artifact_root.mkpath
-    system "unzip", "-q", cached_download, "-d", artifact_root
-
-    artifacts = [
-      [
-        "preternatural-executable.zip",
-        "preternatural.artifactbundle/preternatural/bin/preternatural",
-        "preternatural",
-      ],
-      [
-        "preternaturald-daemon.zip",
-        "preternaturald.artifactbundle/preternaturald/bin/preternaturald",
-        "preternaturald",
-      ],
-    ]
-
-    artifacts.each do |archive, executable_path, installed_name|
-      destination = artifact_root/archive.delete_suffix(".zip")
-      destination.mkpath
-      system "unzip", "-q", artifact_root/archive, "-d", destination
-      bin.install destination/executable_path => installed_name
-    end
+    bin.install "preternatural" => "preternatural"
+    bin.install "preternaturald" => "preternaturald"
   end
 
   service do
