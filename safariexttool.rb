10bb16565ae2880871414f858ce9595571b56aba
Vatsal Manot <5306992+vmanot@users.noreply.github.com>
2026-08-12 11:26:23 +0530

    Release safariexttool 0.1.0

--- safariexttool.rb
+++ safariexttool.rb
@@ -2,7 +2,8 @@ class Safariexttool < Formula
   desc "Inspect and repair Safari web extension registration"
   homepage "https://github.com/PreternaturalAI/browser-extension"
   url "https://github.com/PreternaturalAI/homebrew-preternatural/releases/download/safariexttool-0.1.0/safariexttool.zip"
-  sha256 "7f7faaaf63c6fa0a7ccef3d4d88106f0cbfa3244f21be7abbbdc2697f07684c7"
+  sha256 "c21e05264f640d620dbbe59717e6eebc6c32bcde93e006fe2811af0ed0fbfdb9"
+  depends_on macos: :sonoma
 
   def install
     artifact_root = buildpath/"swift-brew-artifacts"
